41b52fbdd1f2873a64e6c3799b5d5088249157c2
GMMemon875 <ghulammemon875@gmail.com>
2024-07-23 00:19:43 +0500

    Add some Comments to understand code

--- src/componanats/List.jsx
+++ src/componanats/List.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const List = () => {
   const [Title, seTitle] = useState(""); // useState jis men title ke value save he
   const [Description, setDescription] = useState(""); // useState jis men Discription  ke value save he
-  const [List, setList] = useState([]);
+  const [List, setList] = useState([]); // list title and discription List name ke array men stor ho
 
   // jo neche form banaya he us form men ham ni Title and discription ko save keya he
   const submitHandler = (e) => {
@@ -31,17 +31,20 @@ const List = () => {
       </h1>
     );
   if (List.length > 0) {
+    // our ye if condition tab ie chale ge jab input men kuj lekha hoga agr lingth 0 se uper ho matlb kuch lekha ho
     TaskRender = List.map((t, i) => {
-      // taskReander men hm jo be list men se ham map kare gi wo add hoga ab jo object
+      // taskReander men hm jo be list men se ham map kare gi wo add hoga ab jo array men Jo be tast he us ko  hm ni (t) and (i) Array men index
       return (
         <>
           <div key={i} className="bg-slate-500">
             <div className="flex justify-between text-2xl mr-11 ml-11 mt-2 text-white ">
               <h1>{t.Title}</h1>
+              {/* t.title matlb jo be hm ni uper wale input men lekha he wo render hoga */}
               <h3>{t.Description}</h3>
+              {/* t.description matlb jo be hm ni description wale input men lekha he wo render hoga */}
               <button
                 onClick={() => {
-                  deleteHandelar(i);
+                  deleteHandelar(i); // jo be index input hoie hen us ko delete wale onclick se sab remove kardo
                 }}
                 className=" bg-orange-600 w-28  text-wrap p-2 rounded-md"
               >
